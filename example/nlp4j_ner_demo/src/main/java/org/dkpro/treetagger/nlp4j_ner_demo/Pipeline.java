4b070605b2b8364fdbc6066785b078b4c1fb0b01
Shoeb Joarder <shoebjoarder@outlook.com>
2020-06-15 15:28:22 +0200

    added second pipeline for pos tagging

--- example/nlp4j_ner_demo/src/main/java/org/dkpro/treetagger/nlp4j_ner_demo/Pipeline.java
+++ example/nlp4j_ner_demo/src/main/java/org/dkpro/treetagger/nlp4j_ner_demo/Pipeline.java
@@ -10,33 +10,38 @@ import de.tudarmstadt.ukp.dkpro.core.io.text.TextReader;
 import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiWriter;
 import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpPosTagger;
 import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpSegmenter;
-import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpNamedEntityRecognizer;
+//import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpNamedEntityRecognizer;
 
 
 
 public class Pipeline {
 
   public static void main(String[] args) throws Exception {
-		String text_doc = "document.txt";;
-		String out_xmi = "out.xml";
-		String ts_xml = "ts.xml";
+	  
+	  String text_doc = "document.txt";;
+	  String output1_xmi = "output1.xmi";
+	  String ts1_xml = "ts1.xml";
 		
-	    runPipeline(
-	        createReaderDescription(TextReader.class,
-	            TextReader.PARAM_SOURCE_LOCATION, text_doc,
-	            TextReader.PARAM_LANGUAGE, "en"),
-	        createEngineDescription(OpenNlpSegmenter.class),
-	        createEngineDescription(OpenNlpPosTagger.class),
-	        createEngineDescription(OpenNlpNamedEntityRecognizer.class,
-	        		OpenNlpNamedEntityRecognizer.PARAM_VARIANT, "person"),
-	        createEngineDescription(OpenNlpNamedEntityRecognizer.class,
-	        		OpenNlpNamedEntityRecognizer.PARAM_VARIANT, "organization"),
-//	        createEngineDescription(XmiWriter.class,
-//	        		XmiWriter.PARAM_TARGET_LOCATION, "."));
-			createEngineDescription(XmiWriter.class,
-					XmiWriter.PARAM_TARGET_LOCATION, out_xmi,
-					XmiWriter.PARAM_TYPE_SYSTEM_FILE, ts_xml),
-			createEngineDescription(Conll2006Writer.class,
-		            Conll2006Writer.PARAM_TARGET_LOCATION, "."));
+	  runPipeline(
+			  createReaderDescription(TextReader.class,
+					  TextReader.PARAM_SOURCE_LOCATION, text_doc,
+					  TextReader.PARAM_LANGUAGE, "en"),
+        
+			  createEngineDescription(OpenNlpSegmenter.class),
+        
+//			  createEngineDescription(OpenNlpPosTagger.class),
+	        
+//			  createEngineDescription(OpenNlpNamedEntityRecognizer.class,
+//					  OpenNlpNamedEntityRecognizer.PARAM_VARIANT, "person"),
+//			  createEngineDescription(OpenNlpNamedEntityRecognizer.class,
+//					  OpenNlpNamedEntityRecognizer.PARAM_VARIANT, "organization"),
+//			  createEngineDescription(XmiWriter.class,
+//					  XmiWriter.PARAM_TARGET_LOCATION, "."));
+			  
+			  createEngineDescription(XmiWriter.class,
+					  XmiWriter.PARAM_TARGET_LOCATION, output1_xmi,
+					  XmiWriter.PARAM_TYPE_SYSTEM_FILE, ts1_xml),
+			  createEngineDescription(Conll2006Writer.class,
+					  Conll2006Writer.PARAM_TARGET_LOCATION, "."));
   }
 }
---
+++ example/nlp4j_ner_demo/src/main/java/org/dkpro/treetagger/nlp4j_ner_demo/Pipeline2.java
@@ -0,0 +1,39 @@
+package org.dkpro.treetagger.nlp4j_ner_demo;
+
+
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+import static org.apache.uima.fit.factory.CollectionReaderFactory.createReaderDescription;
+import static org.apache.uima.fit.pipeline.SimplePipeline.runPipeline;
+
+import de.tudarmstadt.ukp.dkpro.core.io.conll.Conll2006Writer;
+//import de.tudarmstadt.ukp.dkpro.core.io.text.TextReader;
+import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiReader;
+import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiWriter;
+import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpPosTagger;
+//import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpSegmenter;
+//import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpNamedEntityRecognizer;
+
+
+
+public class Pipeline2 {
+
+  public static void main(String[] args) throws Exception {
+
+		String in_xmi = "output2.xmi";
+		String output3_xmi = "output3.xmi";
+		String ts3_xml = "ts3.xml";
+		
+	    runPipeline(
+	        createReaderDescription(XmiReader.class,
+        		XmiReader.PARAM_SOURCE_LOCATION, in_xmi,
+        		XmiReader.PARAM_LANGUAGE, "en"),
+	        
+	        createEngineDescription(OpenNlpPosTagger.class),
+	        
+			createEngineDescription(XmiWriter.class,
+					XmiWriter.PARAM_TARGET_LOCATION, output3_xmi,
+					XmiWriter.PARAM_TYPE_SYSTEM_FILE, ts3_xml),
+			createEngineDescription(Conll2006Writer.class,
+		            Conll2006Writer.PARAM_TARGET_LOCATION, "."));
+  }
+}
