5d24b22daac2cec20f6781a66c5768a917c53753
Shoeb Joarder <shoebjoarder@outlook.com>
2020-06-18 23:53:43 +0200

    clean up

--- example/nlp4j_ner_demo/src/main/java/org/dkpro/treetagger/nlp4j_ner_demo/Pipeline.java
+++ example/nlp4j_ner_demo/src/main/java/org/dkpro/treetagger/nlp4j_ner_demo/Pipeline.java
@@ -4,36 +4,28 @@ package org.dkpro.treetagger.nlp4j_ner_demo;
 import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
 import static org.apache.uima.fit.factory.CollectionReaderFactory.createReaderDescription;
 import static org.apache.uima.fit.pipeline.SimplePipeline.runPipeline;
-
 import de.tudarmstadt.ukp.dkpro.core.io.conll.Conll2006Writer;
 import de.tudarmstadt.ukp.dkpro.core.io.text.TextReader;
 import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiWriter;
-import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpPosTagger;
 import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpSegmenter;
-//import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpNamedEntityRecognizer;
-
-
 
 public class Pipeline {
 
   public static void main(String[] args) throws Exception {
 	  
 	  String text_doc = "document.txt";;
-	  String output1_xmi = "output1.xmi";
-	  String ts1_xml = "ts1.xml";
-	  String conll1 = "doc1.conll";
 		
 	  runPipeline(
-			  createReaderDescription(TextReader.class,
-					  TextReader.PARAM_SOURCE_LOCATION, text_doc,
-					  TextReader.PARAM_LANGUAGE, "en"),
+		  createReaderDescription(TextReader.class,
+			  TextReader.PARAM_SOURCE_LOCATION, text_doc,
+			  TextReader.PARAM_LANGUAGE, "en"),
         
-			  createEngineDescription(OpenNlpSegmenter.class),
+		  createEngineDescription(OpenNlpSegmenter.class),
        			  
-			  createEngineDescription(XmiWriter.class,
-					  XmiWriter.PARAM_TARGET_LOCATION, output1_xmi,
-					  XmiWriter.PARAM_TYPE_SYSTEM_FILE, ts1_xml),
-			  createEngineDescription(Conll2006Writer.class,
-					  Conll2006Writer.PARAM_TARGET_LOCATION, conll1));
+		  createEngineDescription(XmiWriter.class,
+			  XmiWriter.PARAM_TARGET_LOCATION, "OpenNlpSegmenter",
+			  XmiWriter.PARAM_TYPE_SYSTEM_FILE, "TypeSystem.xml"),
+		  createEngineDescription(Conll2006Writer.class,
+			  Conll2006Writer.PARAM_TARGET_LOCATION, "."));
   }
 }
